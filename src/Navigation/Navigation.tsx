65d9d6c0d1b17d1211630578b2ccddedec6938a9
Igor <spirit-drive@yandex.ru>
2022-04-09 22:42:08 +0300

    Home raw

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -1,10 +1,11 @@
 import React, { FC } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from '../screens/Home';
 
 export const Navigation: FC = () => (
   <BrowserRouter>
     <Routes>
-      <Route index element={<div>Home</div>} />
+      <Route index element={<Home />} />
     </Routes>
   </BrowserRouter>
 );
---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,4 @@
+import React, { FC } from 'react';
+import s from './Home.module.sass';
+
+export const Home: FC = () => <div className={s.root}>Home</div>;
